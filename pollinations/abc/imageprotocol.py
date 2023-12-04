32668a904248d3346393a30dcd4f3b4e883d7825
pollinations.ai <151645294+pollinations-ai@users.noreply.github.com>
2023-12-04 13:19:39 -0500

    Update imageprotocol.py

--- pollinations/abc/imageprotocol.py
+++ pollinations/abc/imageprotocol.py
@@ -8,12 +8,16 @@ Classes:
 from typing import Protocol
 
 
-class ImageProtocol(Protocol):
+class ImageProtocol(Protocol): # 
     """
     ImageProtocol: Image protocol for the ImageObject class.
     """
 
     prompt: str
+    model: str
+    width: int
+    height: int
+    seed: int
     url: str
     date: str
     content: bin
