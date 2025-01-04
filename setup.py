533a6ae624f43b071a3aa8630d48be8c9ef01a66
pollinations.ai <151645294+pollinations-ai@users.noreply.github.com>
2025-01-04 12:58:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(f"{path_absolute}/pollinations/__init__.py", "r") as file:
             break
 
 setup(
-    name="pollinations",
+    name="pollinations.ai",
     version=version,
     description="pollinations.ai | Free AI Text & Image Generation",
     long_description=Path(f"{path_absolute}/README.md").read_text(encoding="utf-8"),
@@ -20,20 +20,22 @@ setup(
     author_email="git.pollinations.ai@gmail.com",
     license="MIT",
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities",
-        "Typing :: Typed",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 4 - Beta",
+        "Natural Language :: English"
     ],
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     python_requires=">=3.7",
-    keywords=["pollinations", "pollinations.ai", "pollinations-ai", "pollinations_ai"],
+    keywords=["pollinations", "pollinations.ai", "pollinations-ai", "pollinations_ai", "ai", "api", "sdk", "wrapper", "free"],
     install_requires=[
         "chardet"
     ],
