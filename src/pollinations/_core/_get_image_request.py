0b8b9193a84e09b53cc6376b37691439ab44971b
pollinations.ai <151645294+pollinations-ai@users.noreply.github.com>
2025-04-16 05:25:30 -0400

    Create _get_image_request.py

---
+++ src/pollinations/_core/_get_image_request.py
@@ -0,0 +1,69 @@
+from .._utils._prepare_request import _prepare_request
+from ..types import (
+    Client,
+    AsyncClient,
+    Params,
+    Request,
+    Response,
+    Args,
+    Kwargs,
+)
+from ._consts import (
+    DEFAULT_HEADERS,
+    DEFAULT_MAX_RETRIES,
+    DEFAULT_TIMEOUT,
+)
+
+__all__: list[str] = ["_get_image_request", "_get_async_image_request"]
+
+
+def _get_image_request(
+    client: Client, params: Params, *args: Args, **kwargs: Kwargs
+) -> Response:
+    params = _prepare_request(params)
+
+    prompt = params.pop("__iprompt")
+    for k, v in params.items():
+        prompt += f"{k}={v}&"
+    prompt = prompt[:-1]
+
+    client.headers = DEFAULT_HEADERS
+    request: Request = client.post(
+        f"prompt/{prompt}", *args, **kwargs, timeout=DEFAULT_TIMEOUT
+    )
+    request.raise_for_status()
+
+    if request.status_code != 200:
+        for _ in range(DEFAULT_MAX_RETRIES):
+            request = client.post(
+                f"prompt/{prompt}", *args, **kwargs, timeout=DEFAULT_TIMEOUT
+            )
+            if request.status_code == 200:
+                break
+    return request
+
+
+async def _get_async_image_request(
+    client: AsyncClient, params: Params, *args: Args, **kwargs: Kwargs
+) -> Response:
+    params = _prepare_request(params)
+
+    prompt = params.pop("__iprompt")
+    for k, v in params.items():
+        prompt += f"{k}={v}&"
+    prompt = prompt[:-1]
+
+    client.headers = DEFAULT_HEADERS
+    request: Request = await client.post(
+        f"prompt/{prompt}", *args, **kwargs, timeout=DEFAULT_TIMEOUT
+    )
+    request.raise_for_status()
+
+    if request.status_code != 200:
+        for _ in range(DEFAULT_MAX_RETRIES):
+            request = await client.post(
+                f"prompt/{prompt}", *args, **kwargs, timeout=DEFAULT_TIMEOUT
+            )
+            if request.status_code == 200:
+                break
+    return request
