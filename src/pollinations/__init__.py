d8690d066c6952798bdae7af4cfbf2d1046bef75
pollinations.ai <151645294+pollinations-ai@users.noreply.github.com>
2025-04-16 05:16:31 -0400

    Create __init__.py

---
+++ src/pollinations/__init__.py
@@ -0,0 +1,103 @@
+from .types import (
+    PILImage,
+    Prompt,
+    Model,
+    Seed,
+    Private,
+    File,
+    Referrer,
+    Stream,
+    Filename,
+    ImageData,
+    Params,
+    EmptyParams,
+    StreamData,
+    ErrorMessage,
+    Args,
+    Kwargs,
+    Negative,
+    Width,
+    Height,
+    NoLogo,
+    Enhance,
+    Safe,
+    Save,
+    System,
+    Contextual,
+    Messages,
+    Role,
+    Message,
+    JsonMode,
+    ReasoningEffort,
+    Tools,
+    ToolChoice,
+    Voice,
+    UseOpenAIEndpoint,
+    Images,
+    ImageFormat,
+    Output,
+    URL,
+    Response,
+    Request,
+    Client,
+    AsyncClient,
+    TextModel,
+    ImageModel,
+    Payload
+)
+from .errors import (
+    EmptyFileError,
+    FailedToStreamError,
+    FailedToTranscribeError,
+    FileNotFoundError,
+    ImproperPermissionsError,
+    InvalidOrUnknownPromptError,
+    NotAFileError
+)
+from ._core import (
+    BaseClass,
+    get_client,
+    get_async_client,
+)
+from .core import (
+    text,
+    image
+)
+from .helpers import get_latest
+from .version import (
+    __title__,
+    __version__,
+    __license__,
+    __description__
+)
+
+
+__all__ = [
+    "PILImage", "Prompt", "Model", "Seed", "Private", "File", "Referrer", "Stream", "Filename",
+    "ImageData", "Params", "EmptyParams", "StreamData", "ErrorMessage", "Args", "Kwargs",
+    "Negative", "Width", "Height", "NoLogo", "Enhance", "Safe", "Save", "System", "Contextual",
+    "Messages", "Role", "Message", "JsonMode", "ReasoningEffort", "Tools", "ToolChoice", "Voice",
+    "UseOpenAIEndpoint", "Images", "ImageFormat", "Output", "URL", "Response", "Request", "Client",
+    "AsyncClient", "TextModel", "ImageModel", "Payload",
+
+    "EmptyFileError", "FailedToStreamError", "FailedToTranscribeError", "FileNotFoundError",
+    "ImproperPermissionsError", "InvalidOrUnknownPromptError", "NotAFileError",
+
+    "BaseClass", "get_client", "get_async_client", 
+    
+    "text", "image",
+    
+    "get_latest",
+    
+    "__title__", "__version__", "__license__", "__description__"
+]
+
+__locals = locals()
+for __name in __all__:
+    if not __name.startswith("__"):
+        try:
+            __locals[__name].__module__ = "pollinations"
+        except (TypeError, AttributeError):
+            pass
+
+get_latest()
