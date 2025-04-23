c43c0f599913dfe4451003073d5d0d44cfedb7ab
flowa <152752280+flowa-ai@users.noreply.github.com>
2025-04-22 22:24:06 -0400

    Fix tool logic

--- src/pollinations/_utils/_clean_params.py
+++ src/pollinations/_utils/_clean_params.py
@@ -5,7 +5,13 @@ __all__: list[str] = ["_clean_params"]
 
 def _clean_params(params: Params, *args: Args, **kwargs: Kwargs) -> Params:
     for k in list(params.keys()):
-        if k.startswith("_") or k == "openai_endpoint":
+        if k.startswith("_") or k == "openai_endpoint" or k == "status":
             params.pop(k)
-
+            
+    if "tools" in params and "tool_choices" in params:
+        if params["tools"] == []:
+            params.pop("tools")
+        if params["tool_choices"] == []:
+            params.pop("tool_choices")
+            
     return params
